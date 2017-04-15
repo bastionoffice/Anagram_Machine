e60061019b0c5eb1b3c7dd1ed22074ac6d1c9b79
bastionoffice <bastionoffice@gmail.com>
2017-04-15 15:27:51 -0400

    Fixed mysql connection
    had set it back to local, now pointing at dev-db again

--- search.php
+++ search.php
@@ -4,8 +4,8 @@ require 'Solver.php';
 
 try{
     // You probably want to change this
-    $db = new PDO('mysql:host=localhost;dbname=anagram', 'root', 'root');
-// $db = new PDO('mysql:host=dev-db.c17ziqujsqqm.ca-central-1.rds.amazonaws.com;dbname=anagram', 'admin', 'K0rgl3s!');
+    //$db = new PDO('mysql:host=localhost;dbname=anagram', 'root', 'root');
+ $db = new PDO('mysql:host=dev-db.c17ziqujsqqm.ca-central-1.rds.amazonaws.com;dbname=anagram', 'admin', 'K0rgl3s!');
 
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // Throw exceptions
 }catch(PDOException $e){
