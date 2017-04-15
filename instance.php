bfd6cca14cbe2b5f4583d6f29924f56dbed36c86
bastionoffice <bastionoffice@gmail.com>
2017-04-15 16:20:28 -0400

    placed before function

--- instance.php
+++ instance.php
@@ -1,7 +1,8 @@
 <?php
+	echo "determining server instance ID";
 $this_instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 if(!empty($this_instance_id))
-	echo(string)($this_instance_id) "This is server instance";
+	echo(string)($this_instance_id);
 else
 	echo "Sorry, instance id unknown";
 ?>
