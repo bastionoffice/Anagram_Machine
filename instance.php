053614ed77389ba5f67990e428808642b54c1883
bastionoffice <bastionoffice@gmail.com>
2017-04-15 16:28:48 -0400

    trying whole file as php

--- instance.php
+++ instance.php
@@ -1,5 +1,5 @@
 <?php
-	echo "determining server instance ID =";
+	echo "determining server instance ID = ";
 $this_instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 if(!empty($this_instance_id))
 	echo(string)($this_instance_id);
