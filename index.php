01bfbe6ffdd270f3d8277ddaf5f257493cc82d1b
bastionoffice <bastionoffice@gmail.com>
2017-04-22 16:53:43 -0400

    moving instance

--- index.php
+++ index.php
@@ -31,10 +31,7 @@ body{background-image:url("data:image/png;base64,	iVBORw0KGgoAAAANSUhEUgAAAGQAAA
 				<span class="count">150,762 words</span>
 			</div>
 			
-			<div style="text-align:center; right: 25%; width:200px; position:absolute;">
-				<name="a"> 
-				<?php include ('instance.php');?><br />
-			</div>
+			
 		</form>
 
 		<div id="loading">
@@ -48,10 +45,10 @@ body{background-image:url("data:image/png;base64,	iVBORw0KGgoAAAANSUhEUgAAAGQAAA
 			<br />
 			<br />
 			<br />
-		<div style="text-align:center; width:400px; position:absolute;">
+		<div style="text-align:center; right: 35%; width:400px; position:absolute;">
 				<name="a"> 
 				<?php include ('instance.php');?><br />
-		</div>
+			</div>
 	</div>
 
 </body>
