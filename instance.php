507305b54045745c37a45d24ef7aca75786e83e5
bastionoffice <bastionoffice@gmail.com>
2017-04-23 09:49:28 -0400

    shortened output length

--- instance.php
+++ instance.php
@@ -1,5 +1,5 @@
 <?php
-	echo "From LAMP server instance ID ";
+	echo "Server Instance ID ";
 $this_instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 if(!empty($this_instance_id))
 	echo(string)($this_instance_id);
