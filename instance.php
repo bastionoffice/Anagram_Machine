b748a02378c5868d479f1f800747fccced30c764
bastionoffice <bastionoffice@gmail.com>
2017-06-17 12:56:02 -0400

    Revert "Added Date to see changes pushed"
    This reverts commit 22e9b845f163361dfff9b1af0929659b06434ed9.

--- instance.php
+++ instance.php
@@ -3,7 +3,6 @@
 $this_instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 if(!empty($this_instance_id))
 	echo(string)($this_instance_id);
-	echo "June 2017";
 else
 	echo "Sorry, instance id unknown";
 ?>
