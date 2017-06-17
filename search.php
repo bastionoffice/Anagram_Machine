dad18fa5f6054661fa797e5cb4ecdc0d37e138e1
bastionoffice <bastionoffice@gmail.com>
2017-06-17 16:08:52 -0400

    typo in creds

--- search.php
+++ search.php
@@ -6,7 +6,7 @@ try{
     // You probably want to change this
     //$db = new PDO('mysql:host=localhost;dbname=anagram', 'root', 'root');
 	//$db = new PDO('mysql:host=anagram00.c17ziqujsqqm.ca-central-1.rds.amazonaws.com;dbname=anagram01', 'Ceesawlstuff', 'K0rgl3s!');
-	$db = new PDO('mysql:host=anagram00.c17ziqujsqqm.ca-central-1.rds.amazonaws.com;dbname=anagram01', 'admin', 'anythingnew!');
+	$db = new PDO('mysql:host=anagram00.c17ziqujsqqm.ca-central-1.rds.amazonaws.com;dbname=anagram01', 'admin', 'anythingnew');
 
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // Throw exceptions
 }catch(PDOException $e){
